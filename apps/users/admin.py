bbc032e351d7bdaad5dabbc993822424ace4fcbd
Zechariah <zechariahadebayo42@gmail.com>
2025-02-25 18:16:50 +0100

    chore: added profile to admin panel

--- apps/users/admin.py
+++ apps/users/admin.py
@@ -1,5 +1,5 @@
 from django.contrib import admin
-from apps.users.models import User
+from apps.users.models import User, Profile
 
-admin.site.register(User)
+admin.site.register((User, Profile))
 
